a5c3bdee3f411be31ef233cec26d9a555e373bd3
Momoka-Igarashi-yamazaki-eu <56525170+MomokaIgarashi@users.noreply.github.com>
2023-07-04 15:20:40 +0200

    side bar options with UI materials

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import ListAltIcon from "@mui/icons-material/ListAlt";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+ 
 function Sidebar() {
   return (
-    <div className='sidebar'>
-      
+    <div className="sidebar">
+      {/* Twitter Icon */}
+      <TwitterIcon className="sidebar--twitterIcon" />
+ 
+      {/* SidebarOption */}
+      <SidebarOption text="Home" Icon={HomeIcon} />
+      <SidebarOption text="Search Topic" Icon={SearchIcon} />
+      <SidebarOption text="Notification" Icon={NotificationsNoneIcon} />
+      <SidebarOption text="Message" Icon={MailOutlineIcon} />
+      <SidebarOption text="Bookmark" Icon={BookmarkBorderIcon} />
+      <SidebarOption text="List" Icon={ListAltIcon} />
+      <SidebarOption text="Profile" Icon={PermIdentityIcon} />
+      <SidebarOption text="See More" Icon={MoreHorizIcon} />
+ 
+      {/* Tweet button */}
     </div>
-  )
+  );
 }
-
-export default Sidebar
+ 
+export default Sidebar;
