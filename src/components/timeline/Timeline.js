f17e621cb1cb63f8ff2ccfb7f219fa3f1395bef3
Momoka-Igarashi-yamazaki-eu <56525170+MomokaIgarashi@users.noreply.github.com>
2023-07-05 09:58:20 +0200

    timeline style

---
+++ src/components/timeline/Timeline.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Timeline.css"
+
+function Timeline() {
+  return (
+    <div className='timeline'>
+        {/* Header */}
+        <div className='timeline--header'>
+            <h2>Home</h2>
+        </div>
+
+        {/* Tweet Box */}
+
+        {/* Post */}
+      
+    </div>
+  )
+}
+
+export default Timeline
